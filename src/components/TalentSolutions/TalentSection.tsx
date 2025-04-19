509764e097062e9404d78649ecc53b9207ce83bd
Jyoti Bhagat <205052781+infotechlaunch@users.noreply.github.com>
2025-04-19 17:09:01 +0530

    talent solution list structure change

--- src/components/TalentSolutions/TalentSection.tsx
+++ src/components/TalentSolutions/TalentSection.tsx
@@ -267,7 +267,7 @@ const TalentSolutions: React.FC = () => {
                                 <div className="w-full relative capitalize space-y-10" data-aos="fade-left">
 
                                     <div className="bg-white lg:p-8 md:p-6 p-3 border border-[#5556D1]/10">
-                                        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                                        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                                             {services.map((service, index) => (
                                                 <div key={index} className="border-2 p-6 space-y-2 bg-white border-[#f13c3c]/20 rounded-[12px] hover:bg-white hover:shadow-lg">
                                                     <div className="w-16 h-16 mx-auto">
