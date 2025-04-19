2345fe6d74de53617cee7a928407c2fbd79dd205
Jyoti Bhagat <205052781+infotechlaunch@users.noreply.github.com>
2025-04-19 16:11:50 +0530

    social media page some ui changes

--- src/pages/SocialMedia.tsx
+++ src/pages/SocialMedia.tsx
@@ -307,7 +307,7 @@ const SocialMedia: React.FC = () => {
                             <div className="grid grid-cols-1 md:grid-cols-4 gap-x-12 gap-y-4 what-love-style text-center">
                                 {/* Feature 1 */}
                                 <div className="bg-white p-6 rounded-xl hover:shadow-xl transition duration-300">
-                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 rounded-full">
+                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 icon-rounded-1">
                                         {/* Clock Icon */}
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -321,7 +321,7 @@ const SocialMedia: React.FC = () => {
 
                                 {/* Feature 2 */}
                                 <div className="bg-white p-6 rounded-xl hover:shadow-xl transition duration-300">
-                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 rounded-full">
+                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 icon-rounded-1">
                                         {/* Bar Chart Icon */}
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
@@ -335,7 +335,7 @@ const SocialMedia: React.FC = () => {
 
                                 {/* Feature 3 */}
                                 <div className="bg-white p-6 rounded-xl hover:shadow-xl transition duration-300">
-                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 rounded-full">
+                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 icon-rounded-1">
                                         {/* Users Icon */}
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -349,7 +349,7 @@ const SocialMedia: React.FC = () => {
 
                                 {/* Feature 4 */}
                                 <div className="bg-white p-6 rounded-xl hover:shadow-xl transition duration-300">
-                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 rounded-full">
+                                    <div className="bg-gradient-to-r from-red-400 to-blue-400 p-3 icon-rounded-1">
                                         {/* Line Chart Icon */}
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
